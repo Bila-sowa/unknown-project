3cc3991207bd0015700567d854a818142c961ef0
Bila_sowa <zlepkopavlo9@gmail.com>
2026-04-30 16:44:58 +0300

    2 Comit

---
+++ script.js
@@ -0,0 +1,8 @@
+var PL = "Programming Language:";
+var PLs = ["Python", "JavaScript", "C++","C#","Html","CSS","C","Typescript"];
+console.log(PL);
+console.log(PLs);
+console.warn("This is beta version of the website, so there may be some bugs. If you find any, please report them to us.");
+
+
+console.error("This website stopped working, because of some errors. Please try again later.");
