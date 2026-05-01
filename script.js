345de5b419efd61adfb19b1b62e47553dc245726
Bila_sowa <zlepkopavlo9@gmail.com>
2026-05-01 21:05:38 +0300

    Test JS

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 var PL = "Programming Language:";
 var PLs = ["Python", "JavaScript", "C++","C#","Html","CSS","C","Typescript"];
+
+// var num1 = 15;
+// var num2 = 7;
+// num1 *= 10;
+
+// var res = num1 - num2;
+
+// // console.log("Result:" + res);
+// console.log("Result: " + (num1 - num2));
+// console.log("Result: " + (num1 + num2));
+// console.log("Result: " + (num1 * num2));
+// console.log("Result: " + (num1 / num2));
+// console.log("Result: " + (num1 % num2));
+
+
+// var num3 = 5;
+// var num4 = ("10");
+// console.log(num3 + Number(num4));
+
+
+
+Math.random();
+console.log("Random Number: " + Math.random());
+
+
+var pi = Math.PI;
+console.log("Number pi: " + pi);
+
+var e = Math.E;
+console.log("Number E: " + e);
+
+var min = Math.min(5, 10, 15);
+console.log("Minimum: " + min);
+
+var max = Math.max(5, 10, 15);
+console.log("Maximum: " + max);
+
+
 console.log(PL);
 console.log(PLs);
 console.warn("This is beta version of the website, so there may be some bugs. If you find any, please report them to us.");
