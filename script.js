8df66adc7a009ab127fe2b6b52bc7e37feb5c86b
Bila_sowa <zlepkopavlo9@gmail.com>
2026-05-02 15:01:52 +0300

    Testing JS

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var PL = "Programming Language:";
 var PLs = ["Python", "JavaScript", "C++","C#","Html","CSS","C","Typescript"];
+console.log(PL);
+console.log(PLs);
+console.warn("This is beta version of the website, so there may be some bugs. If you find any, please report them to us.");
+console.error("This website stopped working, because of some errors. Please try again later.");
+
+// -------------------------------------------------------------------------------------------------------------------------------
 
 // var num1 = 15;
 // var num2 = 7;
@@ -37,10 +43,61 @@ console.log("Minimum: " + min);
 var max = Math.max(5, 10, 15);
 console.log("Maximum: " + max);
 
-
-console.log(PL);
-console.log(PLs);
-console.warn("This is beta version of the website, so there may be some bugs. If you find any, please report them to us.");
+var infinity = Infinity;
+console.log("Infinity: " + infinity);
 
 
-console.error("This website stopped working, because of some errors. Please try again later.");
+// var number1 = 14;
+
+// var number2 = 3;
+
+// if (number1 == 14) {
+//     console.log("Number 1 is equal to 14");
+// }
+// // else if = elif(python)
+// else if (number1 > 14) {
+//     console.log("Number 1 is greater than 14");
+// }
+// else {
+//     console.log("Number 1 is less than 14");
+// }
+
+// age = input("Please enter your age: ");
+var age;
+var is18yearsold;
+
+
+
+if (age >= 18) {
+    is18yearsold = true;
+}
+else {
+    is18yearsold = false;
+}
+
+if (is18yearsold || age >= 18) {
+    console.log("You are 18 years old or older, you can enter the website.");
+}
+else {
+    console.log("You are not 18 years old, you cannot enter the website.");
+}
+
+word = "Hello";
+
+switch (word) {
+    case "Hello":
+        console.log("You said Hello");
+        break;
+    case "Hi":
+        console.log("You said Hi");
+        break;
+    case "Hey":
+        console.log("You said Hey");
+        break;
+    case "word":
+        console.log("You said word");
+        break;
+    default:
+        console.log("You said something else");
+        break;
+}
