7edae9461b2d8bd509b0bee832c594519eacbacd
Bila_sowa <zlepkopavlo9@gmail.com>
2026-05-03 18:27:49 +0300

    Testing JS

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ console.log(PL);
 console.log(PLs);
 console.warn("This is beta version of the website, so there may be some bugs. If you find any, please report them to us.");
 console.error("This website stopped working, because of some errors. Please try again later.");
+var hr = "------------------------------------------------------------------------------"; 
 
-// -------------------------------------------------------------------------------------------------------------------------------
 
 // var num1 = 15;
 // var num2 = 7;
@@ -25,7 +25,7 @@ console.error("This website stopped working, because of some errors. Please try 
 // var num4 = ("10");
 // console.log(num3 + Number(num4));
 
-
+console.log(hr);
 
 Math.random();
 console.log("Random Number: " + Math.random());
@@ -62,7 +62,9 @@ console.log("Infinity: " + infinity);
 //     console.log("Number 1 is less than 14");
 // }
 
+console.log(hr);
 // age = input("Please enter your age: ");
+
 var age;
 var is18yearsold;
 
@@ -82,6 +84,7 @@ else {
     console.log("You are not 18 years old, you cannot enter the website.");
 }
 
+console.log(hr);
 word = "Hello";
 
 switch (word) {
@@ -101,3 +104,37 @@ switch (word) {
         console.log("You said something else");
         break;
 }
+
+
+console.log(hr);
+
+var data = [4,3,6,"Hello",true,5.5,[true,false]];
+
+data[0] = 10
+console.log("Data elements: "+ data.length);
+console.log(data[0]);
+
+// var matrix = [
+//     [5,6],
+//     // Hello World
+//     ["world",false],
+//     [true,3.14]
+// ];
+
+// matrix[1][0] = "Hello";
+// matrix[1][1] = "World";
+
+console.log(hr);
+console.log()
+
+var matrix = [
+    ["Hello", "Hola", "Bonjour"],
+    ["World", "Mundo", "Monde"],
+]
+
+console.log(matrix[0][0] + " " + matrix[1][0]);
+console.log(matrix[0][1] + " " + matrix[1][1]);
+console.log(matrix[0][2] + " " + matrix[1][2]);
+
+
+console.log(hr);
